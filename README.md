5621832e769f3a459839d207c9cbd7625947ed5a
Eddie Maceda <eddiemaceda11@gmail.com>
2022-10-20 14:26:47 -0400

    Revisit RPS project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # rock-paper-scissors
 
 This is my first attempt at the Rock, paper, scissors project from TOP!
+
+I am revisiting the RPS project on a branch!
--- script.js
+++ script.js
@@ -1,50 +1,21 @@
-const arr = ["rock", "paper", "scissors"];
+// user makes selection from button on screen
+// 3 buttons needed, one for each selection
+// add Event list that call playRound() with correct playerselection each time btn clicked
+let playerScore = 0;
+let computerScore = 0;
+let arr = ["rock", "paper", "scissors"];
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const div = document.querySelector("div");
+const nav = document.querySelector("nav");
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    roundPlayed = playRound();
-
-    if (roundPlayed === "Player wins") {
-      playerScore += 1;
-    } else if (roundPlayed === "Computer wins") {
-      computerScore += 1;
-    }
-
-    console.log("Player score: " + playerScore);
-    console.log("Computer score: " + computerScore);
-  }
-
-  if (playerScore === computerScore) {
-    console.log("The Match is a draw!");
-  } else if (playerScore > computerScore) {
-    console.log(
-      "Final score: Player - " +
-        playerScore +
-        ", Computer - " +
-        computerScore +
-        ". Player wins the match!"
-    );
-  } else {
-    console.log(
-      "Final score: Player - " +
-        playerScore +
-        ", Computer - " +
-        computerScore +
-        ". Computer wins the match!"
-    );
-  }
+function getComputerChoice() {
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = prompt("CHOOSE ROCK, PAPER, OR SCISSOR").toLowerCase();
-  computerSelection = getComputerChoice();
-
-  console.log("Player choice: " + playerSelection);
-  console.log("Computer choice: " + computerSelection);
-
+  // Write out the game logic
   if (playerSelection === computerSelection) {
     return "TIE GAME";
   } else if (
@@ -52,20 +23,83 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
+    playerScore++;
     return "Player wins";
   } else if (
     (computerSelection === "rock" && playerSelection === "scissors") ||
     (computerSelection === "scissors" && playerSelection === "paper") ||
     (computerSelection === "paper" && playerSelection === "rock")
   ) {
+    computerScore++;
     return "Computer wins";
   } else {
     return "NOT A VALID ENTRY";
   }
 }
 
-function getComputerChoice() {
-  return arr[Math.floor(Math.random() * arr.length)];
+function checkForWinner(playerScore, computerScore) {
+  if (playerScore === 5 || computerScore === 5) {
+    const h2 = document.createElement("h1");
+    h2.textContent = playerScore === 5 ? "Player WINS" : "Computer WINS";
+    h2.style.color = "green";
+    nav.appendChild(h2);
+  }
+}
+
+function game() {
+  rock.addEventListener("click", () => {
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+
+    roundResult = playRound(playerSelection, computerSelection);
+
+    const para = document.createElement("p");
+    const h3 = document.createElement("h3");
+
+    para.textContent = `Player Choice: rock, Computer Choice: ${computerSelection}`;
+    h3.textContent = roundResult;
+
+    div.appendChild(para);
+    div.appendChild(h3);
+
+    checkForWinner(playerScore, computerScore);
+  });
+
+  paper.addEventListener("click", () => {
+    const playerSelection = "paper";
+    const computerSelection = getComputerChoice();
+
+    roundResult = playRound(playerSelection, computerSelection);
+
+    const para = document.createElement("p");
+    const h3 = document.createElement("h3");
+
+    para.textContent = `Player Choice: paper, Computer Choice: ${computerSelection} `;
+    h3.textContent = roundResult;
+
+    div.appendChild(para);
+    div.appendChild(h3);
+
+    checkForWinner(playerScore, computerScore);
+  });
+
+  scissors.addEventListener("click", () => {
+    const playerSelection = "scissors";
+    const computerSelection = getComputerChoice();
+
+    roundResult = playRound(playerSelection, computerSelection);
+
+    const para = document.createElement("p");
+    const h3 = document.createElement("h3");
+
+    para.textContent = `Player Choice: scissors, Computer Choice: ${computerSelection} `;
+    h3.textContent = roundResult;
+
+    div.appendChild(para);
+    div.appendChild(h3);
+
+    checkForWinner(playerScore, computerScore);
+  });
 }
 
 game();
